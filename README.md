fdf807522cc1a722a43eb8663af875022dc7a7dc
Filipe Câncio <filipe.cancio@outlook.com>
2023-10-30 20:00:17 -0300

    confgirando o main

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # pedro-assistant
+
+
+## Condifgurar o venv
+
+```bash
+python -m venv venv
+.\venv\Scripts\Activate.ps1
+```
+
+## Rodar o projeto
+Execute o Requirments com o seguinte comando:
+
+```bash
+pip install -r requirements.txt
+```
+
+Ou instale as dependencias com os comandos:
+
+```bash
+pip install SpeechRecognition
+pip install pyaudio
+pip install --user gTTS
+pip install --user playsound
+```
--- src/main.py
+++ src/main.py
@@ -1,26 +1,10 @@
 from assistant.audio_catcher import AudioCatcher
-from domain.dog import Dog
-from domain.cat import Cat
+from assistant.assistant import Assistant
 
 if __name__ == '__main__':
-    fido = Dog("Fido")
-    print(fido.eat())
-    print(fido.seeHungry())
-    print(fido.eat())
-    print(fido.seeHungry())
-    #audio_catcher = AudioCatcher()
+    audio_catcher = AudioCatcher()
+    assistant = Assistant()
 
-    #example = audio_catcher.play_recorded_audio("/util/audio/agua.wav")
-    #print(example)
-
-#if __name__ == "__main__":
-    #fido = Dog("Fido")
-    #duda = Cat("Duda")
-    #print(fido.drink())
-    #print(fido.eat())
-    #print(fido.poop())
-    #print(fido.peed())
-    #print(duda.drink())
-    #print(duda.eat())
-    #print(duda.poop())
-   # print(duda.peed())
+    while True:
+        audio_recorded = audio_catcher.listen
+        assistant.doComand(audio_recorded)
